822673ad89026fe393b5c8c3e2d331173881cd75
Max Marchesini <88802441+JavaSoftwareEntwickler@users.noreply.github.com>
2022-04-02 12:38:55 +0200

    Add files via upload

---
+++ Persistenza.java
@@ -0,0 +1,72 @@
+package Esercizio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Exercise: Write the "persistence" function, which takes a positive integer
+ * parameter and returns its "multiplicative persistence", which is the number
+ * of times you have to multiply digits with each other until you have a single
+ * character.
+ * 
+ * examples: persistence(39) == 3 because 39 = 27, 27 = 14, 14=4 and 4 is one
+ * digit
+ * 
+ * persistence(999) == 4 because 999 = 729, 729 = 126, 126 = 12, 12 = 2
+ * 
+ * persistence(4) == 0 because 4 is already a single digit
+ */
+
+public class Persistenza {
+
+	public static void main(String[] args) {
+
+		System.out.println(persistenza(679));
+	}
+
+	public static Integer persistenza(Integer num) {
+
+		return (num < 0 || num > 0 && num < 10) ? 0 : calcolaPersistenza(num);
+
+	}
+
+	public static Integer calcolaPersistenza(Integer num) {
+
+		int persistenza = 0;
+		int lung = num.toString().length();
+		List<Integer> numeri = new ArrayList<>(lung);
+		Integer prodotto = 1;
+
+		for (int i = 0; i < lung; i++) {
+			numeri.add(intAt(num, i));
+			prodotto *= numeri.get(i);
+		}
+		persistenza++;
+		while (prodotto > 9) {
+
+			numeri.clear();
+			for (int i = 0; i < prodotto.toString().length(); i++) {
+				numeri.add(intAt(prodotto, i));
+			}
+
+			prodotto = 1;
+			for (int i = 0; i < numeri.size(); i++) {
+				prodotto *= numeri.get(i);
+			}
+			persistenza++;
+		}
+		return persistenza;
+	}
+
+	/**
+	 * prerequisito paramenti: numeri interi e positivi
+	 * 
+	 * @param numero intero
+	 * @param indice di interesse Integer
+	 * @return singola cifra del numero passato come parametro presente nella
+	 *         posizione dell'indice passato come parametro
+	 */
+	static int intAt(Integer numero, int indice) {
+		return numero.toString().charAt(indice) - 48;
+	}
+}
