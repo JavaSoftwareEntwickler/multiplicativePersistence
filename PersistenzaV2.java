9f50bf0893bccddbb856dc0677fb363b9a3608de
Max Marchesini <88802441+JavaSoftwareEntwickler@users.noreply.github.com>
2024-04-13 21:32:49 +0200

    Update PersistenzaV2.java

--- PersistenzaV2.java
+++ PersistenzaV2.java
@@ -1,6 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Exercise: Write the "persistence" function, which takes a positive integer
+ * parameter and returns its "multiplicative persistence", which is the number
+ * of times you have to multiply digits with each other until you have a single
+ * character.
+ * 
+ * examples: persistence(39) == 3 because 
+ * 1) 3*9 = 27, 
+ * 2) 2*7 = 14,
+ * 3) 1*4 = 4 
+ * 4 is a single digit
+ * 
+ * persistence(999) == 4 because :
+ * 1) 9*9*9 = 729, 
+ * 2) 7*2*9 = 126,
+ * 3) 1*2*6 = 12,
+ * 4) 1*2 = 2 
+ * 2 is a single digit
+ *
+ * persistence(4) == 0 because 4 is already a single digit
+ *
+ */
+
 public class Persistenza {
 
     public static void main(String[] args) {
